6d7bb745a8312c13443f141a03fd9ba8064b0fed
Hansol Jeong <hansol89j@gmail.com>
2021-12-14 00:22:52 +0900

    Dummy data insert test

---
+++ src/main/java/com/hj/blog/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hj.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hj.blog.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+}
