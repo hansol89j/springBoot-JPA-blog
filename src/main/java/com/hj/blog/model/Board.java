602094f1259e963ee00badde692371a4288f51d4
Hansol Jeong <hansol89j@gmail.com>
2022-04-10 23:07:21 +0900

    Find the comment by board id

--- src/main/java/com/hj/blog/model/Board.java
+++ src/main/java/com/hj/blog/model/Board.java
@@ -16,6 +16,8 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -45,6 +47,7 @@ public class Board {
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다. 
 	
 	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+	@JsonIgnoreProperties({"board"})
 	private List<Reply> reply;
 	
 	@CreationTimestamp
